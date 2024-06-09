27178c7ebbf2b43fee76a4d03a28cf6d33d226d9
Awalhossain <waqin21@gmail.com>
2024-06-10 00:36:28 +0600

    refactor: Update config file to include client and server URLs

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -7,6 +7,8 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  clientUrl:process.env.CLIENT_URL, 
+  serverUrl:process.env.SERVER_URL,
   database_url: process.env.DATABASE_URL,
   default_student_pass: process.env.DEFAULT_STUDENT_PASS,
   default_faculty_pass: process.env.DEFAULT_FACULTY_PASS,
