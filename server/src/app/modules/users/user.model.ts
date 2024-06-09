002ca72e038dc862cd642d5ad09ec1a733178bc7
Awalhossain <waqin21@gmail.com>
2024-06-10 00:36:07 +0600

    feat: Update user model and schema

--- server/src/app/modules/users/user.model.ts
+++ server/src/app/modules/users/user.model.ts
@@ -1,9 +1,7 @@
-import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
-import config from '../../../config';
 import { IUser, UserModel } from './user.interface';
 
-const UserSchema = new Schema<IUser>({
+const authSchema = new Schema<IUser>({
   name: {
     type: String,
     required: true,
@@ -17,71 +15,17 @@ const UserSchema = new Schema<IUser>({
   password: {
     type: String,
     required: true,
+    select: false
   },
   role: {
     type: String,
     enum: ['user', 'admin'],
     default: 'user',
   },
-  readingList: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Book',
-    },
-  ],
+  token:{
+    type:String
+  }
 
-  finishedBooks: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Book',
-    },
-  ],
-  userPrefernce: [
-    {
-      book: {
-        type: Schema.Types.ObjectId,
-        ref: 'Book',
-      },
-      status: {
-        type: String,
-        enum: ['read', 'reading', 'finished'],
-      },
-      updatedAt: {
-        type: Date,
-      },
-    },
-  ],
 });
 
-UserSchema.statics.isUserExist = async function (
-  email: string
-): Promise<IUser | null> {
-  return await User.findOne(
-    { email },
-    {
-      email: 1,
-      _id: 1,
-      role: 1,
-    }
-  );
-};
-
-UserSchema.statics.isPasswordMatch = async function (
-  givenPassword: string,
-  savePassword: string
-): Promise<boolean> {
-  return await bcrypt.compare(givenPassword, savePassword);
-};
-
-UserSchema.pre('save', async function (next) {
-  const user = this as IUser;
-
-  user.password = await bcrypt.hash(
-    user.password,
-    Number(config.bycrypt_salt_rounds)
-  );
-
-  next();
-});
-
-export const User = model<IUser, UserModel>('User', UserSchema);
+export const User = model<IUser, UserModel>('auth', authSchema);
