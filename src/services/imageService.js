f4821eee944c585e9528d5ee59a5a47ba73272c2
Awalhossain <waqin21@gmail.com>
2024-06-10 17:36:24 +0600

    refactor: Update image URLs in imageService.js

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,104 +1,78 @@
 const mockImages = [
   {
     id: 1,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-1368496779-1024x1024.jpg",
     description: "Graduation Ceremony",
   },
   {
     id: 2,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/college-phot/timothy-kassis-Qj-5RbUb1UE-unsplash.jpg",
     description: "Student Life",
   },
   {
     id: 3,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-1340342476-1024x1024.jpg",
     description: "Campus Tour",
   },
-  // Add more mock images as needed
   {
     id: 4,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-1739895622-1024x1024.jpg",
     description: "Student Housing",
   },
   {
     id: 5,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-1456729829-1024x1024.jpg",
     description: "Library",
   },
   {
     id: 6,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/sports/jeffrey-f-lin-CUK8i7lr3l8-unsplash.jpg",
     description: "Sports Facilities",
   },
   {
     id: 7,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/sports/philippa-rose-tite-93YYkxujyus-unsplash.jpg",
     description: "Dining Hall",
   },
   {
     id: 8,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-1353372838-1024x1024.jpg",
     description: "Classroom",
   },
   {
     id: 9,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/istockphoto-597957846-1024x1024.jpg",
     description: "Laboratory",
   },
   {
     id: 10,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/sports/istockphoto-1278976828-1024x1024.jpg",
     description: "Campus Map",
   },
   {
     id: 11,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/sports/riley-mccullough-iezcEpGuYdE-unsplash.jpg",
     description: "Student Center",
   },
   {
     id: 12,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/college-phot/university-6699377_1280.jpg",
     description: "Bookstore",
   },
   {
     id: 13,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/college-phot/vadim-sherbakov-d6ebY-faOO0-unsplash.jpg",
     description: "Student Clubs",
   },
   {
     id: 14,
-    url: "https://via.placeholder.com/500",
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/sports/braden-collum-ttbCwN_mWic-unsplash.jpg",
     description: "Academic Building",
   },
   {
-    id: 18,
-    url: "https://via.placeholder.com/500",
+    id: 15,
+    url: "https://video-gka8dxaqb0babzct.z01.azurefd.net/events/pang-yuhao-_kd5cxwZOK4-unsplash.jpg",
     description: "Research Center",
-  },
-  {
-    id: 19,
-    url: "https://via.placeholder.com/500",
-    description: "Student Services",
-  },
-  {
-    id: 20,
-    url: "https://via.placeholder.com/500",
-    description: "Health Center",
-  },
-  {
-    id: 21,
-    url: "https://via.placeholder.com/500",
-    description: "Career Services",
-  },
-  {
-    id: 22,
-    url: "https://via.placeholder.com/500",
-    description: "Alumni Association",
-  },
-  {
-    id: 23,
-    url: "https://via.placeholder.com/500",
-    description: "International Students",
   },
 ];
 
