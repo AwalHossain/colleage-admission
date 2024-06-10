4c0a12fd0a3bd8d7c6a2489a13c89133dd5833f7
Awalhossain <waqin21@gmail.com>
2024-06-10 17:35:36 +0600

    refactor: Update SignUpPage component with form validation and error handling

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,81 @@
-import { SignUp } from "@clerk/clerk-react";
 
-const SignUpPage = () => (
-  <SignUp path="/sign-up" signInUrl="sign-in" />
-);
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { fetchRegister } from "../services/authService";
+
+
+const SignUpPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    try {
+      const res = await fetchRegister( username,email, password);
+      console.log(res,'res from login');
+  
+      if(res.data.statusCode === 200){
+        setLoading(false);
+        toast.success("Account created successfully");
+      } 
+  
+      console.log(res.data, "just err checking");
+    } catch (error) {
+      setLoading(false);
+      console.error(error.response.status, "error from auth service");
+      if(!error.response.data.success){
+        toast.error("Error: "+error.response.data.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up for an account</h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleRegister}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm space-y-4 ">
+            <div>
+              <label htmlFor="user-name" className="sr-only">Name</label>
+              <input id="user-name" name="user-name" type="text" autoComplete="user-name" required className="appearance-none rounded-3xl relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name" value={username} onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div>
+              <label htmlFor="email-address" className="sr-only">Email address</label>
+              <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-3xl relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" value={email} onChange={e => setEmail(e.target.value)} />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">Password</label>
+              <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-3xl relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <Link to="/sign-in" className="font-medium text-indigo-600 hover:text-indigo-500 hover:underline">
+                Already have an account? Sign in
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-3xl text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default SignUpPage;
