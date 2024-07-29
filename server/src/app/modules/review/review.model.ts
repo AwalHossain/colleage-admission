ff7c1a49e6dfc4f648a336938ea34f5977aee083
Awalhossain <waqin21@gmail.com>
2024-07-29 16:56:19 +0600

    refactor: Update review model to use consistent naming for college and user references

--- server/src/app/modules/review/review.model.ts
+++ server/src/app/modules/review/review.model.ts
@@ -4,12 +4,11 @@ import mongoose, { Schema } from "mongoose";
 
 
 const reviewSchema = new Schema({
-    collegeId: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
-    author: { type: String, required: true },
+    college: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
     rating: { type: Number, required: true },
     content: { type: String, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-})
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Auth', required: true },
+});
 
 
 export const Review = mongoose.model('Review', reviewSchema)
