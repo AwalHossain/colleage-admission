8e79c979c1c5321410ab2aa0eb5e13827733680f
Awalhossain <waqin21@gmail.com>
2024-07-31 16:54:32 +0600

    refactor: Update admission model to include user reference

--- server/src/app/modules/admission/admission.model.ts
+++ server/src/app/modules/admission/admission.model.ts
@@ -3,14 +3,15 @@ import mongoose, { Schema } from "mongoose";
 
 
 const admissionSchema = new Schema({
-    collegeId: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
+    college: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
     name: { type: String, required: true },
     subject: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
     address: { type: String, required: true },
     dob: { type: Date, required: true },
-    image: { type: String }
+    image: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Auth', required: true },
 })
 
 
