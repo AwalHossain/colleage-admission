3cea22d0e772de7c8f2452db9764d673311bad2a
Awalhossain <waqin21@gmail.com>
2024-08-01 16:51:34 +0600

    refactor: Add lazy loading for social media login buttons

--- Client/src/services/authService.js
+++ Client/src/services/authService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_API_URL } from "../config";
+import { getFromLocalStorage } from "../lib/local-storage";
 
 export const fetchLogin = async (email, password) => {
   try {
@@ -18,9 +19,26 @@ export const fetchLogin = async (email, password) => {
 export const fetchRegister = async (name, email, password) => {
   try {
     const response = await axios.post(`${BASE_API_URL}/auth/create`, {
+      name,
       email,
       password,
     });
+    return response;
+  } catch (error) {
+    console.error(error.response.data);
+    throw error;
+  }
+};
+export const fetchSocialLogin = async (name, email, photoURL) => {
+  try {
+    const response = await axios.post(
+      `${BASE_API_URL}/auth/social-media-login`,
+      {
+        email,
+        name,
+        photoURL,
+      }
+    );
     return response;
   } catch (error) {
     console.error(error.response.data);
@@ -48,6 +66,23 @@ export const resetPassword = async (password, id, token) => {
         password,
       }
     );
+    return response;
+  } catch (error) {
+    console.error(error, "error from auth service");
+    throw error;
+  }
+};
+
+export const getMe = async () => {
+  const token = getFromLocalStorage("token");
+
+  console.log(token, "token from auth service");
+  try {
+    const response = await axios.get(`${BASE_API_URL}/auth/me`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
     return response;
   } catch (error) {
     console.error(error, "error from auth service");
