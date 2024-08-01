e452d69930480679b5d34044ccc0327f936c6664
Awalhossain <waqin21@gmail.com>
2024-08-01 16:51:29 +0600

    refactor: Add lazy loading for images in admissionService.js

--- Client/src/services/admissionService.js
+++ Client/src/services/admissionService.js
@@ -1,6 +1,43 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { BASE_API_URL } from "../config";
+import { getFromLocalStorage } from "../lib/local-storage";
+
 // src/services/admissionService.js
 export const submitAdmission = async (formData) => {
   // Simulate network delay
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("Submitted admission form:", formData);
+  const token = getFromLocalStorage("token");
+  console.log(formData, "form data from admission service");
+
+  try {
+    const response = await axios.post(
+      `${BASE_API_URL}/admission/create`,
+      formData,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error, "error from auth service");
+    throw error;
+  }
+};
+
+export const getAdmissions = async () => {
+  const token = getFromLocalStorage("token");
+  try {
+    const response = await axios.get(`${BASE_API_URL}/admission/get`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error, "error from auth service");
+    toast.error("Error: " + "Something went wrong while fetching admissions");
+    throw error;
+  }
 };
