a06da29962b2f954ec67ab8ff2d3b3c27785ee17
Awalhossain <waqin21@gmail.com>
2024-08-05 16:49:35 +0600

    refactor: Update Home page to import Reviews component from the correct directory

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 import ImageGallery from "../components/ImageGallery";
 import ResearchPapers from "../components/ResearchPapers";
-import Reviews from "../components/Review";
+
+import Reviews from "../components/review/Review";
 import { fetchColleges } from "../services/collegeService";
 import Hero from "./Hero";
+
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const { data: colleges, isLoading } = useQuery("colleges", fetchColleges);
@@ -19,17 +21,6 @@ const Home = () => {
       <Hero />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-12">
-
-
-
-          {/* {searchTerm && filteredColleges?.length > 0 && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-              {filteredColleges.map((college) => (
-                <CollegeCard key={college.id} college={college} />
-              ))}
-            </div>
-          )} */}
-
           <ImageGallery />
           <ResearchPapers />
           <Reviews />
