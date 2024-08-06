50bfac7085f6fbb016d6aed332586e7b9e2854af
Awalhossain <waqin21@gmail.com>
2024-08-06 16:43:01 +0600

    refactor: Add PrivateRoutes, local storage utility functions, loading spinner, DropdownMenu, StarRating, PostReview, ResetPassword, and ForgetPassModal components

--- Client/src/pages/Admission.jsx
+++ Client/src/pages/Admission.jsx
@@ -1,35 +1,73 @@
-import { TrashIcon } from '@heroicons/react/outline'
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useMutation } from 'react-query'
-import { submitAdmission } from '../services/admissionService'
-import CollegeAutoSuggestion from './CollegeAutoSuggestion'
-import SubjectAutoSuggestion from './SubjectAutoSuggestion'
+import { TrashIcon } from "@heroicons/react/outline";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useMutation } from "react-query";
 
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Spinner } from "../lib/loading";
+import { getFromLocalStorage, setToLocalStorage } from "../lib/local-storage";
+import { submitAdmission } from "../services/admissionService";
+import useAuth from "../zustand/authStore";
+import CollegeAutoSuggestion from "./CollegeAutoSuggestion";
+import SubjectAutoSuggestion from "./SubjectAutoSuggestion";
 
 const Admission = () => {
   const [imageUrl, setImageUrl] = useState("");
+  const navigation = useNavigate();
+  const {isSignedIn}  = useAuth();
 
   const [isLoading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
-    collegeName: '',
-    name: '',
-    subject: '',
-    email: '',
-    phone: '',
-    address: '',
-    dob: '',
+    collegeName: "",
+    _id: "",
+    name: "",
+    subject: "",
+    email: "",
+    phone: "",
+    address: "",
+    dob: "",
     image: null,
+  });
+
+  const mutation = useMutation(submitAdmission,{
+      onSuccess:() => {
+        toast.success("Review Added Successfully");
+        setFormData({
+          collegeName: "",
+          _id: "",
+          name: "",
+          subject: "",
+          email: "",
+          phone: "",
+          address: "",
+          dob: "",
+        }); 
+
+        navigation.navigate("/my-college");
+        
+        },
+        onError: (error) => {
+          console.log(error,"checking from");
+          if (error && 'status' in error && error.status === 401) {
+              toast.error("You're not authorized! login to continue");
+          } else if (error  && error.response.data.message) {
+              // Handle specific error messages from the server
+              toast.error(error.data.message);
+          } else {
+              // Handle general or unknown errors
+              toast.error("An unknown error occurred");
+          }
+          },
   })
 
-  const { mutate, isLoading:MutateLoading, isSuccess } = useMutation(submitAdmission)
+  const {isLoading:MutateLoading, error,data,isSuccess} = mutation;
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleImageChange = (e) => {
-
     const file = e.target.files[0];
     let body = new FormData();
     body.set("key", "f4c465c3ffeda9a61a1dc3b8c28f0047");
@@ -43,8 +81,10 @@ const Admission = () => {
           setLoading(false);
           setImageUrl(response.data.data.display_url);
           setFormData({ ...formData, image: response.data.data.display_url });
+          setToLocalStorage("image", response.data.data.display_url);
         } else {
           setLoading(false);
+          toast.error("Error: " + "Something went wrong while uploading image");
           console.error("Error:", response.data.status);
           setImageUrl("");
         }
@@ -54,159 +94,221 @@ const Admission = () => {
         setLoading(false);
         setImageUrl("");
       });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.image) {
+      toast.error("Please upload an image");
+      return;
+    }
+
+    try {
+    mutation.mutate(formData)
+    } catch (err) {
+      console.error("Error: from sadmiadsfo", err.response.data);
+      toast.error(err.response.data.message || err.message);
+    }
+  };
+
+  useEffect(() => {
+    const image = getFromLocalStorage("image");
+    if (image) {
+      setImageUrl(image);
+      formData.image = image;
+    }
+  }, []);
+
+
+  if (!isSignedIn) {
+    navigation.navigate("/sign-in");
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    mutate(formData)
-  }
-
-
-
-
 
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="max-w-4xl w-full mx-auto p-8 bg-white shadow-md rounded-lg">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 mb-6">
-          Admission Application
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div>
-              <label htmlFor="collegeName" className="block text-sm font-medium text-gray-700">
-                College Name
-              </label>
-              <CollegeAutoSuggestion setFormData={setFormData} formData={formData} />
-            </div>
-            <div>
-              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="mt-1 p-2 border-2  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="subject" className="block text-sm font-medium text-gray-700">
-                Subject
-              </label>
-              <SubjectAutoSuggestion setFormData={setFormData} formData={formData} />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                Phone
-              </label>
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                value={formData.phone}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                Address
-              </label>
-              <input
-                type="text"
-                name="address"
-                id="address"
-                className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                value={formData.address}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="dob" className="block text-sm font-medium text-gray-700">
-                Date of Birth
-              </label>
-              <input
-                type="date"
-                name="dob"
-                id="dob"
-                className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                value={formData.dob}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="image" className="block text-sm font-medium text-gray-700">
-                Your Profile Photo
-              </label>
-
-              {
-                imageUrl && (
-                  <div className="relative mt-1 w-32 h-30">
-                    <img src={imageUrl} alt="preview" className=" object-cover" />
-                    <span className='absolute top-0 right-0 text-red-600'>
-                      <TrashIcon className='h-5 w-5 cursor-pointer' onClick={() => setImageUrl('')} />
-                    </span>
+    <>
+      {MutateLoading ? (
+        <Spinner className="middle" />
+      ) : (
+        <>
+          <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <div className="max-w-4xl w-full mx-auto p-8 bg-white shadow-md rounded-lg">
+              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 mb-6">
+                Admission Application
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                  <div>
+                    <label
+                      htmlFor="collegeName"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      College Name
+                    </label>
+                    <CollegeAutoSuggestion
+                      setFormData={setFormData}
+                      formData={formData}
+                    />
                   </div>
-                )
-              }
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="mt-1 p-2 border-2  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={formData.name}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="subject"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Subject
+                    </label>
+                    <SubjectAutoSuggestion
+                      setFormData={setFormData}
+                      formData={formData}
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      type="tel"
+                      name="phone"
+                      id="phone"
+                      className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={formData.phone}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Address
+                    </label>
+                    <input
+                      type="text"
+                      name="address"
+                      id="address"
+                      className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={formData.address}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="dob"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Date of Birth
+                    </label>
+                    <input
+                      type="date"
+                      name="dob"
+                      id="dob"
+                      className="mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={formData.dob}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="image"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Your Profile Photo
+                    </label>
 
-              {
-                isLoading && (
-                  <p className="mt-1 text-sm text-gray-500">Uploading...</p>
-                ) 
-              }
+                    {imageUrl && (
+                      <div className="relative mt-1 w-32 h-30">
+                        <img
+                          src={imageUrl}
+                          alt="preview"
+                          className=" object-cover"
+                        />
+                        <span className="absolute top-0 right-0 text-red-600">
+                          <TrashIcon
+                            className="h-5 w-5 cursor-pointer"
+                            onClick={() => setImageUrl("")}
+                          />
+                        </span>
+                      </div>
+                    )}
 
-              <input
-                type="file"
-                name="image"
-                id="image"
-                className={`mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md ${isLoading || imageUrl ? 'hidden' : ''} `}
-                onChange={handleImageChange}
-                accept="image/*"
-                required
-              />
+                    {isLoading && (
+                      <p className="mt-1 text-sm text-gray-500">Uploading...</p>
+                    )}
+
+                    <input
+                      type="file"
+                      name="image"
+                      id="image"
+                      className={`mt-1 border-2 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md ${
+                        isLoading || imageUrl ? "hidden" : ""
+                      } `}
+                      onChange={handleImageChange}
+                      accept="image/*"
+                    />
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <button
+                    type="submit"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    disabled={isLoading || MutateLoading}
+                  >
+                    {isLoading || MutateLoading ? "Submitting..." : "Submit"}
+                  </button>
+                  {isSuccess && (
+                    <p className="mt-2 text-sm text-green-500">
+                      Application submitted successfully!
+                    </p>
+                  )}
+                </div>
+              </form>
             </div>
           </div>
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              disabled={isLoading || MutateLoading}
-            >
-              {isLoading || MutateLoading ? 'Submitting...' : 'Submit'}
-            </button>
-            {isSuccess && (
-              <p className="mt-2 text-sm text-green-500">
-                Application submitted successfully!
-              </p>
-            )}
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
+        </>
+      )}
+    </>
+  );
+};
 
-export default Admission
+export default Admission;
